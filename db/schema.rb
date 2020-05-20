46cee21e9ac241cf2d6f4053528f182a7ad84d9d
Emmanuel Omona <emmanuelomona1@gmail.com>
2020-05-20 13:24:17 +0200

    users can view the products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_082438) do
+ActiveRecord::Schema.define(version: 2020_05_20_110047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2020_05_13_082438) do
   create_table "jwt_blacklist", force: :cascade do |t|
     t.string "jti", null: false
     t.index ["jti"], name: "index_jwt_blacklist_on_jti"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title", default: "", null: false
+    t.string "subheader"
+    t.string "imageUrl"
+    t.string "imageTitle"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
--- spec/requests/v1/products_spec.rb
+++ spec/requests/v1/products_spec.rb
@@ -18,6 +18,32 @@ RSpec.describe 'products' do
       expect(response).to have_http_status(200)
     end
 
+    it 'returns empty records in the database' do
+      get v1_products_path, params: {}, headers: { 'Authorization': "Bearer #{@token}}" }
+      expected = { products: [] }.to_json
+      expect(response.body).to eq(expected)
+    end
+
+    it 'returns records in the database' do
+      product = { 
+        title: 'Title',
+        subheader: 'Sub-header',
+        imageUrl: "https://res.cloudinary.com/opix/image/upload/v1553531583/samples/cloudinary-icon.png",
+        imageTitle: 'Image Title',
+        description: 'Item description'
+      }
+      Product.create(product)
+      get v1_products_path, params: {}, headers: { 'Authorization': "Bearer #{@token}}" }
+      result = JSON.parse(response.body)['products'].first
+      expect(result['title']).to eq(product[:title])
+      expect(result['subheader']).to eq(product[:subheader])
+      expect(result['imageUrl']).to eq(product[:imageUrl])
+      expect(result['imageTitle']).to eq(product[:imageTitle])
+      expect(result['description']).to eq(product[:description])
+    end
+
+    
+
     it 'unauthorized if invalid token' do
       get v1_products_path, params: {}, headers: { 'Authorization': "Bearer 123}" }
       expect(response).to have_http_status(401)
